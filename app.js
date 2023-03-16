b0a43584bad92d6e84708e48b5c79e59c027d654
Samuel Jim <samueljimp2@gmail.com>
2023-03-15 17:24:38 -0700

    initial update

---
+++ app.js
@@ -0,0 +1,6 @@
+const time = document.getElementById("time");
+greeting = document.getElementById("greeting");
+username = document.getElementById("username");
+focus = document.getElementById("focus");
+content = document.getElementById("content");
+author = document.getElementById("author");
